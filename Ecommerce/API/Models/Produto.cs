90332144bf90cbd49e84bd876a9ff8c241d7cea5
LUAN FREITAS COSTA <luan.freitas@cs.up.edu.br>
2025-09-05 11:29:54 -0300

    aula5

---
+++ Ecommerce/API/Models/Produto.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using System;
+
+namespace API.Models
+{
+    public class Produto
+    {
+        public Produto()
+        {
+            Id = Guid.NewGuid().ToString();  
+            CriadoEm = DateTime.Now;         
+        }
+
+        public string Id { get; set; }
+        public string Nome { get; set; }
+        public double Preco { get; set; }      
+        public int Quantidade { get; set; }    
+        public DateTime CriadoEm { get; set; }
+    }
+}
+
--- Ecommerce/API/Program.cs
+++ Ecommerce/API/Program.cs
@@ -1,6 +1,34 @@
+using API.Models;
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+List<Produto> produtos = new List<Produto>()
+    {
+        new Produto { Nome = "Caneta", Preco = 1.50, Quantidade = 100 },
+        new Produto { Nome = "Caderno", Preco = 15.00, Quantidade = 50 },
+        new Produto { Nome = "Mochila", Preco = 120.00, Quantidade = 20 },
+        new Produto { Nome = "Lápis", Preco = 0.80, Quantidade = 200 },
+        new Produto { Nome = "Borracha", Preco = 2.00, Quantidade = 75 }
+    };
+
+app.MapGet("/", () => "API de Produtos");
+
+app.MapGet("/api/produto/listar", () =>
+{
+    return produtos;
+});
+app.MapPost("/api/produto/cadastrar", (Produto produto) =>
+{
+   
+produtos.Add(produto);  
+   
+});
 
 app.Run();
+
+
+
+
+
+
+
